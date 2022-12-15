bc54e62ed2239832d7c48da8850fddf97595cddc
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-12-15 22:34:57 +0300

    Fixed

--- HomeWork36Collection4/Program.cs
+++ HomeWork36Collection4/Program.cs
@@ -2,90 +2,18 @@
 {
     private static void Main(string[] args)
     {
-        Dictionary<string, string> personnelsAccounts = new Dictionary<string, string>();
-
-        Console.WriteLine("Добро пожаловать в кадровый учёт!\n");
-        Console.WriteLine("Для продолжения нажмите любую клавишу");
-        Console.ReadKey();
-        Console.Clear();
-
-        TakeUserChoice(personnelsAccounts);
-    }
-
-    static void CreateAccount(Dictionary<string, string> personnelsAccounts)
-    {
-        Console.Clear();
-        Console.Write("Введите фамилию с большой буквы: ");
-        string name = Console.ReadLine();
-
-        Console.Write("Введите должность: ");
-        string post = Console.ReadLine();
-
-        personnelsAccounts.Add(name, post);
-
-        Console.WriteLine("\nДосье внесено. Для продолжения нажмите любую клавишу");
-        Console.ReadKey();
-    }
-
-    static void OutputAccount(Dictionary<string, string> personnelsAccounts)
-    {
-        Console.Clear();
-        Console.Write("Введите фамилию с большой буквы: ");
-        string name = Console.ReadLine();
-
-        if (personnelsAccounts.ContainsKey(name))
-        {
-            Console.WriteLine($"{name} - {personnelsAccounts[name]}");
-            Console.WriteLine("\nДля продолжения нажмите любую клавишу");
-            Console.ReadKey();
-        }
-        else
-        {
-            Console.WriteLine("\nТакой фамилии нет. Для продолжения нажмите любую клавишу.");
-            Console.ReadKey();
-        }
-    }
-
-    static void OutputAllAccounts(Dictionary<string, string> personnelsAccounts)
-    {
-        Console.Clear();
-
-        foreach (var accounts in personnelsAccounts)
-        {
-            Console.WriteLine($"{accounts.Key} - {accounts.Value}");
-        }
-
-        Console.WriteLine("\nДля продолжения нажмите любую клавишу");
-        Console.ReadKey();
-    }
-
-    static void DeleteAccount(Dictionary<string, string> personnelsAccounts)
-    {
-        Console.Clear();
-        Console.Write("Введите фамилию  с большой буквы: ");
-        string name = Console.ReadLine();
-
-        if (personnelsAccounts.ContainsKey(name))
-        {
-            personnelsAccounts.Remove(name);
-
-            Console.WriteLine("\nДосье удалёно. Для продолжения нажмите любую клавишу");
-            Console.ReadKey();
-        }
-        else
-        {
-            Console.WriteLine("\nТакой фамилии нет. Для продолжения нажмите любую клавишу.");
-            Console.ReadKey();
-        }
-    }
-
-    static void TakeUserChoice(Dictionary<string, string> personnelsAccounts)
-    {
         const string CreateAccount = "1";
         const string OutputAccount = "2";
         const string OutputAllAccounts = "3";
         const string DeleteAccount = "4";
         const string Exit = "5";
+
+        Dictionary<string, string> dossiers = new Dictionary<string, string>();
+
+        Console.WriteLine("Добро пожаловать в кадровый учёт!\n");
+        Console.WriteLine("Для продолжения нажмите любую клавишу");
+        Console.ReadKey();
+        Console.Clear();
 
         bool isWorking = true;
 
@@ -99,29 +27,100 @@
 
             string userInput = Console.ReadLine();
 
-            if (isWorking)
+            switch (userInput)
             {
-                switch (userInput)
-                {
-                    case CreateAccount:
-                        Program.CreateAccount(personnelsAccounts);
-                        break;
-                    case OutputAccount:
-                        Program.OutputAccount(personnelsAccounts);
-                        break;
-                    case OutputAllAccounts:
-                        Program.OutputAllAccounts(personnelsAccounts);
-                        break;
-                    case DeleteAccount:
-                        Program.DeleteAccount(personnelsAccounts);
-                        break;
-                    case Exit:
-                        isWorking = false;
-                        break;
-                }
+                case CreateAccount:
+                    Program.CreateAccount(dossiers);
+                    break;
+                case OutputAccount:
+                    Program.OutputAccount(dossiers);
+                    break;
+                case OutputAllAccounts:
+                    Program.OutputAllAccounts(dossiers);
+                    break;
+                case DeleteAccount:
+                    Program.DeleteAccount(dossiers);
+                    break;
+                case Exit:
+                    isWorking = false;
+                    break;
             }
 
             Console.Clear();
+        }
+    }
+    static void CreateAccount(Dictionary<string, string> dossiers)
+    {
+        Console.Clear();
+        Console.Write("Введите фамилию с большой буквы: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Введите должность: ");
+        string post = Console.ReadLine();
+
+        if (dossiers.ContainsKey(name))
+        {
+            Console.WriteLine("\nТакая фамилия уже есть. Для продолжения нажмите любую клавишу.");
+            Console.ReadKey();
+        }
+        else
+        {
+            dossiers.Add(name, post);
+            Console.WriteLine("\nДосье внесено. Для продолжения нажмите любую клавишу");
+        }
+           
+        Console.ReadKey();
+    }
+
+    static void OutputAccount(Dictionary<string, string> dossiers)
+    {
+        Console.Clear();
+        Console.Write("Введите фамилию с большой буквы: ");
+        string name = Console.ReadLine().ToLower();
+
+        if (dossiers.ContainsKey(name.ToLower()))
+        {
+            Console.WriteLine($"{name} - {dossiers[name]}");
+            Console.WriteLine("\nДля продолжения нажмите любую клавишу");
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.WriteLine("\nТакой фамилии нет. Для продолжения нажмите любую клавишу.");
+            Console.ReadKey();
+        }
+    }
+
+    static void OutputAllAccounts(Dictionary<string, string> dossiers)
+    {
+        Console.Clear();
+
+        foreach (var account in dossiers)
+        {
+            Console.WriteLine($"{account.Key} - {account.Value}");
+        }
+
+        Console.WriteLine("\nДля продолжения нажмите любую клавишу");
+        Console.ReadKey();
+    }
+
+    static void DeleteAccount(Dictionary<string, string> dossiers)
+    {
+        Console.Clear();
+        Console.Write("Введите фамилию  с большой буквы: ");
+        string name = Console.ReadLine().ToLower();
+
+        if (dossiers.ContainsKey(name.ToLower()))
+        {
+            dossiers.Remove(name);
+
+            Console.WriteLine("\nДосье удалёно. Для продолжения нажмите любую клавишу");
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.WriteLine("\nТакой фамилии нет. Для продолжения нажмите любую клавишу.");
+            Console.ReadKey();
         }
     }
 }
